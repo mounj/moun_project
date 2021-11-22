56f1edc2846fcfa3aff858b67ba13cc3beb79cf8
Julien MOURIN <julien.mourin@gmail.com>
2021-11-22 12:25:48 +0100

    sharing code

---
+++ moun_project/lib.py
@@ -0,0 +1,7 @@
+import sys
+
+def try_me():
+    print('Give me your name')
+    name = 'John' #input('Give me your name:')
+    print(f'Your welcome {name}')
+    return None
---
+++ tests/test_lib.py
@@ -0,0 +1,5 @@
+from moun_project.lib import try_me
+
+
+def test_try_me():
+    assert try_me() == None
